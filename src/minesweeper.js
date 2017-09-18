3e95bb213effefd2ae31f026144948cfc16a1846
Pete Akhurst <peteakhurst@gmail.com>
2017-09-18 22:58:02 +1000

    3rd minesweeper game

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,21 +1,67 @@
-const printBoard = (board) => {
-  console.log("Current Board:");
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+// Function accepts the numberOfRows and numberOfColumns and returns a board with that many rows and columns.
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+
+  let board = [];
+
+  for (let currentRow = 0; currentRow < numberOfRows; currentRow++) {
+
+    let row = [];
+
+      for (let currentColumn = 0; currentColumn < numberOfColumns; currentColumn++){
+        row.push(' ');
+      }
+
+      board.push(row);
+
+  }
+
+  return board;
+
+
 };
 
-const board = [
-  // this is posiion 0
-  [' ', ' ', ' '],
-  // this is posiion 1
-  [' ', ' ', ' '],
-  // this is posiion 2
-  [' ', ' ', ' ']
-];
+console.log(generatePlayerBoard(1,3));
+console.log(generatePlayerBoard(6,3));
+console.log(generatePlayerBoard(5,7));
 
-printBoard(board);
-board[0][0] = 1;
-board[2][2] = "B";
 
-printBoard(board);
+
+
+
+
+
+
+
+
+
+
+
+
+// Old Code
+
+// const generatePlayerBoard = (board) => {
+//
+// };
+
+// const printBoard = (board) => {
+//   console.log("Current Board:");
+//   console.log(board[0].join(' | '));
+//   console.log(board[1].join(' | '));
+//   console.log(board[2].join(' | '));
+// };
+
+// const board = [
+//   // this is posiion 0
+//   [' ', ' ', ' '],
+//   // this is posiion 1
+//   [' ', ' ', ' '],
+//   // this is posiion 2
+//   [' ', ' ', ' ']
+// ];
+//
+// printBoard(board);
+// board[0][0] = 1;
+// board[2][2] = "B";
+//
+// printBoard(board);
